cd6296526f461b91fcdeb4b0ee8e7d861924193e
LucianABC <louabecasis@gmail.com>
2019-11-12 12:00:13 -0300

    Ejercicio terminado

--- objetos.test.js
+++ objetos.test.js
@@ -11,7 +11,8 @@
  * de nuestro carrito. 
  *  
  ***********************************************/
-listaProductos = [];
+let listaProductos = [];
+let shoppingCart = [];
 
 let producto = {
     id: 1, 
@@ -20,7 +21,12 @@ let producto = {
     precio: "precio"
 } 
 
-const cargarProducto= (id, nombreProducto, desc, precio) =>{
+beforeEach (() => {
+    listaProductos = [];
+    shoppingCart = [];
+});
+
+const addToList= (id, nombreProducto, desc, precio) =>{
     let producto= {
         id: id,
         nombre: nombreProducto,
@@ -34,7 +40,7 @@ const cargarProducto= (id, nombreProducto, desc, precio) =>{
 
 
 test('cargar productos en la lista',() => {
-    cargarProducto(9, "nombreProducto", "descripcion", 35)
+    addToList(9, "nombreProducto", "descripcion", 35)
     expect(listaProductos[0]).toStrictEqual({
         id: 9,
         nombre: "nombreProducto",
@@ -42,32 +48,24 @@ test('cargar productos en la lista',() => {
         precio: 35
     });
 });
-
-/****************** ***********************
+/****************** B U S C A R *********************/
 
 const searchID = (ID) => {
 
     for (let item of listaProductos) {
         if (item.id == ID){
-            
+            return item
         }
-    };
-    indice = listaProductos.indexOf(ID);
-    
-    if (indice == -1) {
-        throw "El producto no esta en la lista"    
-    } else {
-        return listaProductos[indice]
-    };
+    }
 };
 
 test ('buscar por id', () => {
-    cargarProducto(9, "remera", "roja", 350);
-    expect(searchID(9)).toBe({id: 9,nombre:"remera", descripcion:"roja", precio:350})
+    addToList(9, "remera", "roja", 350);
+    expect(searchID(9)).toStrictEqual({id: 9,nombre:"remera", descripcion:"roja", precio:350})
 });
 
 
-****************** M O D I F I C A R*********************/
+/****************** M O D I F I C A R*********************/
 
 const modificarProducto = (ID, newID, newName, newDesc, newPrice) => {
     
@@ -85,7 +83,7 @@ const modificarProducto = (ID, newID, newName, newDesc, newPrice) => {
 
 
 test('modificar productos de la lista', () => {
-    cargarProducto(9,"remera", "roja", 350,50)
+    addToList(9,"remera", "roja", 350,50)
     expect(modificarProducto(9, 2, "pantalon","negro", 3.5)).toStrictEqual({id: 2, nombre: "pantalon", descripcion:"negro",precio: 3.5});
 });
 
@@ -93,16 +91,95 @@ test('modificar productos de la lista', () => {
 /*********************B O R R A R************************/
 
 
-const borrarProducto = () => {
-    
+const borrarProducto = (ID) => {
+    for (let item of listaProductos) {
+        if (item.id == ID){
+            listaProductos= listaProductos.splice(item, 1)
+        }
+
+        return listaProductos
+    }
 };
 
 test('borrar producto de la lista', () => {
-    cargarProducto(10, "falta", "negra", 499,99)
-    expect(borrarProducto()).toStrictEqual();
-
+    addToList(10, "falda", "negra", 499,99);
+    addToList(11, "remera", "negra", 399,99);
+    let previousLength= listaProductos.length;
+    borrarProducto(10);
+    expect(previousLength).toBeGreaterThan(listaProductos.length);
 });
 
+
+/************************A G R E G A R  AL  C A R R I T O***************************/
+
+const addToCart = (ID) => {
+    for (let item of listaProductos) {
+        if (item.id == ID){
+            shoppingCart.push(item);
+        }
+    }
+
+        return shoppingCart
+};
+
+
+
+test('agregar prod al carrito', () => {
+    addToList(10, "falda", "negra", 499.99);
+    addToList(11, "remera", "negra", 399.99);
+    addToCart(10);
+    expect(shoppingCart[0]).toStrictEqual({
+        id: 10,
+        nombre: "falda",
+        descripcion: "negra",
+        precio: 499.99
+    });
+});
+
+/************************B O R R A R  DEL  C A R R I  T O ***************************/
+
+const deleteFromChart = (ID) => {
+    for (let item of shoppingCart) {
+        if (item.id == ID) {
+            shoppingCart.splice(item, 1);
+        }
+    }
+};
+
+test('borrar del carrito', ()=> {
+    addToList(10, "falda", "negra", 499,99);
+    addToList(11, "remera", "negra", 399,99);
+    addToCart(10);
+    addToCart(11);
+    let previousLength = shoppingCart.length
+    deleteFromChart(10);
+    expect(shoppingCart.length).toBeLessThan(previousLength)
+});
+
+/************************S U M A R P R E C I O ***************************/
+
+const totalPrice = () => {
+     let total = 0;
+     for (let item of shoppingCart) {
+        total += item.precio;
+     }
+
+     return total
+
+
+};
+
+
+test ('precio total de compra', () => {
+    addToList(10, "falda", "negra", 400);
+    addToList(11, "remera", "negra", 300);
+    addToCart(10);
+    addToCart(11);
+
+
+    expect(totalPrice()).toBe(700);
+}
+);
 
 
 
